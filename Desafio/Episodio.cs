b894f771469bbc0694aa9b5aa83b6b7afecdc197
natevitor <rm550381@fiap.com.br>
2024-09-09 00:22:47 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio/Episodio.cs
@@ -0,0 +1,23 @@
+﻿class Episodio {
+    private List<string> convidados = new();
+
+    public Episodio(int ordem, string titulo, int duracao) {
+        Ordem = ordem;
+        Titulo = titulo;
+        Duracao = duracao;
+    }
+
+    public int Ordem { get; }
+
+    public string Titulo { get; }
+
+    public int Duracao { get; }
+
+    // Propriedade resumida
+    public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) - {string.Join(", ", convidados)}";
+
+    // Método para adicionar convidados
+    public void AdicionarConvidados(string convidado) {
+        convidados.Add(convidado);
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿Episodio ep1 = new(1, "Tecnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(2, "Tecnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
+Console.WriteLine(ep1.Resumo);
+
+Podcast podcast = new("Podcast especial", "Daniel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirDetalhes();
